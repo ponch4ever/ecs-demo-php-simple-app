cca8962a86c18137189fb97277e9b0c7b4823098
ponch4ever <ponch_4ever@hotmail.com>
2018-10-26 16:19:23 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -16,8 +16,8 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <h2>Thank you!</h2>
+                <p>Ponch! Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
